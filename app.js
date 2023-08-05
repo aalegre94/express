c8047f4b63f7c0c2391d2e06285ef539b65d3145
aalegre94 <alegre.qa@gmail.com>
2023-08-05 08:07:29 -0500

    add midlewares

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("En el midleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("En otro midleware");
+  next();
+});
+
 app.listen(3000);
