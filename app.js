d02366c33412f29f8318216d3af26eb0e4df0d4d
aalegre94 <alegre.qa@gmail.com>
2023-08-06 17:52:46 -0500

    add stage publicar en dockerhub

--- app.js
+++ app.js
@@ -2,16 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+//Para ver el estado del request
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 
-app.use((req, res, next) => {
-  console.log("En el midleware");
-  next();
-});
-
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("En otro midleware");
   res.send("<h1>Hola desde Express</h1>");
 });
