d3b78806858944f6c38aedae635e83c2985fa968
aalegre94 <alegre.qa@gmail.com>
2023-08-11 15:05:28 -0500

    add arrary where save data
    usamos un array donde guardar la data y luego lo usamos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 //Para ver el estado del request
@@ -15,7 +15,7 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
