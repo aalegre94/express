c26853b5d2654f14a82de1f89c9f9822de5c80a2
aalegre94 <alegre.qa@gmail.com>
2023-08-23 12:23:32 -0500

    fixed jenkinsfle and error message

--- models/product.js
+++ models/product.js
@@ -37,13 +37,13 @@ module.exports = class Product {
         const updatedProducts = [...products];
         updatedProducts[existingProductIndex] = this;
         fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-          console.log(err);
+          console.log("save", err);
         });
       } else {
         this.id = Math.random().toString();
         products.push(this);
         fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
+          console.log("save", err);
         });
       }
     });
