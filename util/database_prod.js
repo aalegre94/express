8bc535fc7867525738e6280a121270a35656f3ab
aalegre94 <alegre.qa@gmail.com>
2023-08-24 10:41:46 -0500

    modified conx bd4

--- util/database_prod.js
+++ util/database_prod.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "tiendaNode",
+  host: "dbmysql",
   port: 3306,
   user: "root",
   database: "db",
@@ -13,7 +13,7 @@ pool.getConnection((err, connection) => {
     console.error("Error connecting to MySQL:", err);
     return;
   }
-  console.log("Connected to MySQL database");
+  console.log("Connected to MySQL database :) ");
   connection.release(); // Importante: liberar la conexión después de la verificación
 });
 
