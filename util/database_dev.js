b585ca0de5001d0e2feb50b32475fa08cd6ff8bf
aalegre94 <alegre.qa@gmail.com>
2023-08-27 11:38:11 -0500

    add conexion for sequelize

--- util/database_dev.js
+++ util/database_dev.js
@@ -1,11 +1,18 @@
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
+const Sequilize = require("sequelize");
+const sequelize = new Sequelize("db", "root", "root", {
+  dialect: "mysql",
   host: "localhost",
   port: 3306,
-  user: "root",
-  database: "db",
-  password: "root",
 });
 
-module.exports = pool.promise();
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   database: "db",
+//   password: "root",
+// });
+
+// module.exports = pool.promise();
